0437a3e02c8edfc9a1888c9197e0841bc239309d
RohitSharm23 <rohitsharma93716@gmail.com>
2026-03-03 11:16:28 +0530

    Update script.js

--- unixgame/script.js
+++ unixgame/script.js
@@ -1,6 +1,5 @@
-</script>
+
         function toggleMenu() {
             const buttons = document.getElementById('authButtons');
             buttons.classList.toggle('mobile-active');
         }
-    </script>
